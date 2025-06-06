e430fc734968cb25dcdd821d1c752f7efac3da57
Vadzim Simanau <eks220885@gmail.com>
2025-06-06 21:45:17 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '/js/libraries.js';
+import '/js/header.js';
+import '/js/mobile-menu.js';
+import '/js/about-us.js';
+import '/js/artists.js';
+import '/js/modal-artists.js';
+import '/js/feedback.js';
